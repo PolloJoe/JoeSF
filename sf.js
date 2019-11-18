b9b565666588ba6c0362c2c6a22f4bed0f8d7fbf
PolloJoe <54596666+PolloJoe@users.noreply.github.com>
2019-11-18 10:04:43 -0800

    Add files via upload

---
+++ sf.js
@@ -0,0 +1,24 @@
+var a= document.getElementById("nacho")
+var b= document.getElementById("e")
+var c= document.getElementById("mask")
+var d= document.getElementById("skeleton")
+
+
+
+
+
+
+
+
+
+
+
+
+function changePicture(){
+	a.classList.add ("nachopic")
+}
+
+function changePictures(){
+	b.classList.add ("esqueletopic")
+}
+
